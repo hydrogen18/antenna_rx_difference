fbfaa56b7d70b6f4213ee4204ed46c23105683ba
Eric Urban <hydrogen18@gmail.com>
2020-08-15 21:29:23 -0500

    add more comparisons

--- antenna_rx_difference/__main__.py
+++ antenna_rx_difference/__main__.py
@@ -9,21 +9,47 @@ import tempfile
 import time
 import json
 import os
+import shelve
+import glob
 
 from astropy.coordinates import EarthLocation
 import astropy.units as u
 from astroplan import Observer
 
+import geopy
+import geopy.distance
+
+import maidenhead
+
+GNUPLOT_COLORS = ['black', 'red', 'purple', 'blue', 'green']
+
+MAIDENHEAD_REGEX = re.compile('^[A-Z]{2}[0-9]{2}$')
+
 class SignalObservation(object):
-  __slots__ = ('ts', 'freq', 'direction', 'mode', 'rssi', 'freq_offset', 'msg',)
-  def __init__(self, ts, freq, direction, mode, rssi, _, freq_offset, msg):
+  __slots__ = ('ts', 'freq', 'direction', 'mode', 'snr', 'freq_offset', 'msg',)
+  def __init__(self, ts, freq, direction, mode, snr, _, freq_offset, msg):
     self.ts = ts
     self.freq = freq
     self.direction = direction
     self.mode = mode
-    self.rssi = rssi
+    self.snr = snr
     self.freq_offset = freq_offset
     self.msg = msg
+
+  def __hash__(self):
+    return hash((self.ts, self.freq,  self.msg,))   
+
+  def get_maidenhead(self):
+    data = self.msg.split(' ')
+    if len(data) < 3: # a message should have at least pieces of information, if it includes maidenhead
+      return
+     
+    last_data = data[-1]
+    if last_data == 'RR73': # commonly send to indicate the communication is over
+      return
+
+    if MAIDENHEAD_REGEX.match(last_data):
+      return last_data
 
 class DataMode(enum.IntEnum):
   FT8 = 1
@@ -40,7 +66,7 @@ def parse_wsjtx_output(fin):
     if m is None:
       continue
 
-    ts, freq, direction, mode, rssi, _, freq_offset, msg = m.groups()
+    ts, freq, direction, mode, snr, _, freq_offset, msg = m.groups()
 
     dot = freq.find('.')
     freq_scale = 1
@@ -51,7 +77,7 @@ def parse_wsjtx_output(fin):
 
     freq = int(freq.replace('.', '')) * freq_scale
     freq_offset = int(freq_offset)
-    rssi = int(rssi)
+    snr = int(snr)
     
     mode = mode.lower()
     if mode == 'rx':
@@ -69,7 +95,7 @@ def parse_wsjtx_output(fin):
     seconds = int(ts[11:])
 
     ts = datetime.datetime(year, month, day, hour, minute, seconds)
-    yield SignalObservation(ts, freq, direction, mode, rssi, None, freq_offset, msg)
+    yield SignalObservation(ts, freq, direction, mode, snr, None, freq_offset, msg)
 
 
 tmpA = tempfile.TemporaryFile(mode='w+b')
@@ -105,6 +131,11 @@ observer = Observer(location = EarthLocation.from_geodetic(0.0 * u.deg, 0.0 * u.
 #sunrise = observer.sun_rise_time()
 #sunset = observer.sun_set_time()
 
+def truncate_ts_to_hour_unix(ts):
+  truncated_ts = datetime.datetime(ts.year, ts.month, ts.day, ts.hour)
+  unix_ts = int(time.mktime(truncated_ts.timetuple()))
+  return unix_ts
+
 def find_time_range(inputA, inputB):
   earliest = None
   latest = None
@@ -123,8 +154,7 @@ earliest_time, latest_time = find_time_range(tmpA, tmpB)
 def count_by_freq_and_hour(fin):
   data = {}
   for obj in read_object_sequence(fin):
-    truncated_ts = datetime.datetime(obj.ts.year, obj.ts.month, obj.ts.day, obj.ts.hour)
-    unix_ts = int(time.mktime(truncated_ts.timetuple()))
+    unix_ts = truncate_ts_to_hour_unix(obj.ts)
     k = (unix_ts, obj.freq,)
     v = data.get(k)
     if v is None:
@@ -132,9 +162,7 @@ def count_by_freq_and_hour(fin):
     data[k] = v + 1
   return data
 
-def write_files_for_counts_by_hour(nameA, nameB, tmpA, tmpB):
-  dataA = count_by_freq_and_hour(tmpA)
-  dataB = count_by_freq_and_hour(tmpB)
+def find_all_hours_and_freqs(dataA, dataB):
   all_freqs = set()
   [all_freqs.add(x[1]) for x in dataA.keys()]
   [all_freqs.add(x[1]) for x in dataB.keys()]
@@ -145,6 +173,17 @@ def write_files_for_counts_by_hour(nameA, nameB, tmpA, tmpB):
   all_hours = sorted(list(all_hours))
 
   freq_ordering = sorted(list(all_freqs))
+  return (all_hours, freq_ordering,)
+
+GNUPLOT_TIMEFMT_HRS = "set timefmt \"%Y/%m/%d %H\"\n"
+GNUPLOT_X_FORMAT_HRS = "set format x \"%m-%d\\n%H\"\n"
+DATETIME_FORMAT_FOR_GNUPLOT = '%Y/%m/%d %H'
+
+def write_files_for_counts_by_hour(nameA, nameB, tmpA, tmpB):
+  dataA = count_by_freq_and_hour(tmpA)
+  dataB = count_by_freq_and_hour(tmpB)
+  all_hours, all_freqs = find_all_hours_and_freqs(dataA, dataB)
+
   csv_filename = 'counts_by_hour.csv'
   with open(csv_filename, 'w') as fout:
     fout.write("# input file A: %s\n" % (nameA,))
@@ -154,7 +193,7 @@ def write_files_for_counts_by_hour(nameA, nameB, tmpA, tmpB):
     fout.write("# ")
     cols = ['hour (unix ts)']
     
-    for freq in freq_ordering:
+    for freq in all_freqs:
       cols.append('%d Hz count (%s)' % (freq, nameA,))
       cols.append('%d Hz count (%s)' % (freq, nameB,))
    
@@ -163,8 +202,8 @@ def write_files_for_counts_by_hour(nameA, nameB, tmpA, tmpB):
     fout.write("\n")
 
     for hour in all_hours:
-      row = [datetime.datetime.utcfromtimestamp(hour).strftime('%Y/%m/%d %H')]
-      for freq in freq_ordering:
+      row = [datetime.datetime.utcfromtimestamp(hour).strftime(DATETIME_FORMAT_FOR_GNUPLOT)]
+      for freq in all_freqs:
         k = (hour, freq,)
         v_a = dataA.get(k, 0)
         v_b = dataB.get(k, 0)
@@ -176,7 +215,7 @@ def write_files_for_counts_by_hour(nameA, nameB, tmpA, tmpB):
 
   with open('counts_by_hour.gnuplot', 'w') as fout:
     fout.write("set datafile separator comma\n")
-    for i, freq in enumerate(freq_ordering):
+    for i, freq in enumerate(all_freqs):
       filename = "counts_by_hour_%dHz.png" % (freq,)
       freq_human = "%.3f MHz" % (freq/(10**6.0),)
 
@@ -186,24 +225,261 @@ def write_files_for_counts_by_hour(nameA, nameB, tmpA, tmpB):
       fout.write("set title \"Signals received per hour (%s)\"\n" % (freq_human,))
       fout.write("set xlabel \"Hour\"\n")
       fout.write("set ylabel \"Count\"\n")
-      # TODO legend
-      # TODO x axis as hours
+
       a_column_idx = 1 + (i*2)
       b_column_idx = 1 + (i*2 + 1)
 
       fout.write("set xdata time\n")
-      fout.write("set timefmt \"%Y/%m/%d %H\"\n")
-#      fout.write("set xrange [\"2020/08/01 00\":\"2020/08/31 23\"]\n")
-      fout.write("set format x \"%m-%d\\n%H\"\n")
+      fout.write(GNUPLOT_TIMEFMT_HRS)
+      fout.write(GNUPLOT_X_FORMAT_HRS)
       fout.write("set grid\n")
 
-      # TODO colors
+      # TODO colors that aren't boring
       fout.write("plot \"%s\" using 1:%d with linespoints lw 3 pt 7 ps 2 lc rgb \"black\" title \"%s\", \\\n" % (csv_filename, a_column_idx + 1, nameA,))
       fout.write("     \"%s\" using 1:%d with linespoints lw 3 pt 7 ps 2 lc rgb \"red\" title \"%s\"\n" % (csv_filename, b_column_idx + 1, nameB,))
       fout.write("\n")
 
+
+def maximum_distance_by_hour(rx_maidenhead, fin):
+  rx_coords = maidenhead.to_location(rx_maidenhead)
+  data = {}
+  for obj in read_object_sequence(fin):
+    loc = obj.get_maidenhead()
+    if loc is None:   
+      continue
+    if len(loc) == 4:
+      loc = loc + 'mm' # pick the middle
+
+    tx_coords = maidenhead.to_location(loc)
+    dist_km = geopy.distance.great_circle(rx_coords, tx_coords).km
+    unix_ts = truncate_ts_to_hour_unix(obj.ts)
+    k = (unix_ts, obj.freq,)
+
+    v = data.get(k)
+    if v is None or v < dist_km:      
+      data[k] = dist_km
+  return data
     
+def write_maximum_distance_by_hour(rx_maidenhead, nameA, nameB, tmpA, tmpB):
+  dataA = maximum_distance_by_hour(rx_maidenhead, tmpA)
+  dataB = maximum_distance_by_hour(rx_maidenhead, tmpB)
+  all_hours, all_freqs = find_all_hours_and_freqs(dataA, dataB)
+   
+
+  csv_filename = 'max_distance_by_hour.csv'
+  with open(csv_filename, 'w') as fout:
+    fout.write("# input file A: %s\n" % (nameA,))
+    fout.write("# input file B: %s\n" % (nameB,))
+    fout.write("# hours covered: %d\n" % (len(all_hours),) )
+    fout.write("# frequencies covered: %d\n" % (len(all_freqs),) )
+    fout.write("# ")
+    cols = ['hour (unix ts)']
+    
+    for freq in all_freqs:
+      cols.append('%d Hz max. km. (%s)' % (freq, nameA,))
+      cols.append('%d Hz max. km. (%s)' % (freq, nameB,))
+   
+    fout.write(','.join(cols))
+
+    fout.write("\n")
+
+    for hour in all_hours:
+      row = [datetime.datetime.utcfromtimestamp(hour).strftime(DATETIME_FORMAT_FOR_GNUPLOT)]
+      for freq in all_freqs:
+        k = (hour, freq,)
+        v_a = dataA.get(k, 0)
+        v_b = dataB.get(k, 0)
+        row.append('%.1f' % (v_a,))
+        row.append('%.1f' % (v_b,))
+
+      fout.write(','.join(str(x) for x in row))
+      fout.write("\n")
+
+  with open('max_distance_by_hour.gnuplot', 'w') as fout:
+    fout.write("set datafile separator comma\n")
+    for i, freq in enumerate(all_freqs):
+      filename = "max_dist_by_hour_%dHz.png" % (freq,)
+      freq_human = "%.3f MHz" % (freq/(10**6.0),)
+
+      fout.write("set terminal pngcairo font \"Arial, 16\" size 1920, 1080\n")
+      fout.write("set output \"%s\"\n" % (filename,))
+
+      fout.write("set title \"Maximum reception range by hour (%s)\"\n" % (freq_human,))
+      fout.write("set xlabel \"Hour\"\n")
+      fout.write("set ylabel \"Distance (km.)\"\n")
+
+      a_column_idx = 1 + (i*2)
+      b_column_idx = 1 + (i*2 + 1)
+
+      fout.write("set xdata time\n")
+      fout.write(GNUPLOT_TIMEFMT_HRS)
+      fout.write(GNUPLOT_X_FORMAT_HRS)
+      fout.write("set grid\n")
+
+      # TODO colors that aren't boring
+      fout.write("plot \"%s\" using 1:%d with linespoints lw 3 pt 7 ps 2 lc rgb \"black\" title \"%s\", \\\n" % (csv_filename, a_column_idx + 1, nameA,))
+      fout.write("     \"%s\" using 1:%d with linespoints lw 3 pt 7 ps 2 lc rgb \"red\" title \"%s\"\n" % (csv_filename, b_column_idx + 1, nameB,))
+      fout.write("\n")
+
+class SignalMatcher(object):
+  def __init__(self):
+     tmpname = 'tmp.signalmatcher'
+     if len(tmpname) > 3:
+       for fname in glob.glob(tmpname + '*'):
+         os.unlink(fname)
+     self.db = shelve.open(tmpname)
+
+  def insert(self, cat, obj):
+    k = '%x' % hash(obj)
+    v = self.db.get(k)
+    if v is None:
+      v = {}
+
+    sub_v = v.get(cat)
+    if sub_v is None:
+      sub_v = []
+      v[cat] = sub_v
+    sub_v.append(obj)
+    self.db[k] = v
+
+  def generate_matches(self):
+    freq_tolerance = 100
+    for k in self.db.keys():
+      matched = set()
+
+      data_for_k = self.db[k]
+      
+      categories = list(data_for_k.keys())
+      for cat in categories:        
+        entries = data_for_k[cat]
+        
+        for i, entry in enumerate(entries):
+          # Search every other entry for matches
+          for other_cat in categories:
+            if other_cat == cat:
+              continue
+            for j, other_entry in enumerate(data_for_k[other_cat]):
+              if entry.mode == other_entry.mode and other_entry.freq == other_entry.freq and other_entry.ts == other_entry.ts and entry.msg == other_entry.msg:
+                freq_offset_delta = abs(entry.freq_offset - other_entry.freq_offset)
+                if freq_offset_delta < freq_tolerance: # two receivers may not indicate the same freq offset exactly
+                  match_k = tuple(sorted( [ (cat, i,), (other_cat, j,) ]))
+                  if match_k not in matched:
+                    matched.add(match_k)
+                    match = { cat: entry, other_cat: other_entry }
+                    yield match
+                  
+def write_comparative_snr(nameA, nameB, tmpA, tmpB):
+  sm = SignalMatcher()
+
+  sample_cnt = 0
+  for obj in read_object_sequence(tmpA):
+    sm.insert(nameA, obj)   
+
+  for obj in read_object_sequence(tmpB):
+    sm.insert(nameB, obj)
+
+  snr_sums = {}
+  
+  all_hours = set()
+  all_freqs = set()
+  for m in sm.generate_matches():
+    sample_cnt += 1
+    a = m[nameA]
+    b = m[nameB]
+    snr_diff = b.snr - a.snr
+    unix_ts = truncate_ts_to_hour_unix(a.ts)
+    all_hours.add(unix_ts)
+    all_freqs.add(a.freq)
+    k = (unix_ts, a.freq)
+    v = snr_sums.get(k)
+    if v is None:
+      v = [0, 0.0]
+    v[0] += 1
+    v[1] += snr_diff
+
+    snr_sums[k] = v
+
+
+  snr_means = {}
+  for k, v in snr_sums.items():
+    n = v[0]
+    s = v[1]
+    result = s/n
+    snr_means[k] = result
+
+  del snr_sums
+
+  csv_filename = 'comparative_snr_by_hour.csv'
+  all_hours = sorted(list(all_hours))
+  all_freqs = sorted(list(all_freqs))
+  with open(csv_filename, 'w') as fout:
+    fout.write("# input file A: %s\n" % (nameA,))
+    fout.write("# input file B: %s\n" % (nameB,))
+    fout.write("# hours covered: %d\n" % (len(all_hours),) )
+    fout.write("# frequencies covered: %d\n" % (len(all_freqs),) )
+    fout.write("# signals received by both antennas: %d\n" % (sample_cnt,))
+    fout.write("# ")
+    cols = ['hour (unix ts)']
+    
+    for freq in all_freqs:
+      cols.append('%d Hz relative SNR (mean)' % (freq,))
+   
+    fout.write(','.join(cols))
+
+    fout.write("\n")
+    
+    for hour in all_hours:
+      row = [datetime.datetime.utcfromtimestamp(hour).strftime(DATETIME_FORMAT_FOR_GNUPLOT)]
+      for freq in all_freqs:
+        k = (hour, freq)
+        v = snr_means.get(k)
+        if v is None:
+          row.append('NaN')
+        else:
+          row.append('%.1f' % (v,))
+      fout.write(','.join(str(x) for x in row))
+      fout.write("\n")
+
+  with open('comparative_snr_by_hour.gnuplot', 'w') as fout:
+    fout.write("set datafile separator comma\n")
+    fout.write("set datafile missing \"NaN\"\n")
+    
+    filename = "comparative_snr_by_hour.png" 
+    fout.write("set terminal pngcairo font \"Arial, 16\" size 1920, 1080\n")
+    fout.write("set output \"%s\"\n" % (filename,))
+    fout.write("set title \"Average Relative SNR by hour\"\n")
+    fout.write("set xlabel \"Hour\"\n")
+    fout.write("set ylabel \"SNR above %s\"\n" % (nameA,))
+
+
+    fout.write("set xdata time\n")
+    fout.write(GNUPLOT_TIMEFMT_HRS)
+    fout.write(GNUPLOT_X_FORMAT_HRS)
+    fout.write("set grid\n")
+
+    accum = []
+    for i, freq in enumerate(all_freqs):
+      column_idx = 1 + i
+      freq_human = "%.3f MHz" % (freq/(10**6.0),)
+      # TODO colors that aren't boring
+      if i == 0:
+        fout.write("plot ")
+      else:
+        fout.write("\t")
+
+      fout.write("\"%s\" using 1:%d with linespoints lw 2 pt 7 ps 2 lc rgb \"%s\" title \"%s\"" % (csv_filename, column_idx + 1, GNUPLOT_COLORS[i], freq_human,))
+      if i != len(all_freqs) - 1:
+        fout.write(", \\")
+      fout.write("\n")
+
+
+
+rx_gridsquare = 'EM10dk'
+ 
 write_files_for_counts_by_hour(nameA, nameB, tmpA, tmpB)
+write_maximum_distance_by_hour(rx_gridsquare, nameA, nameB, tmpA, tmpB)
+write_comparative_snr(nameA, nameB, tmpA, tmpB)
+
 
 tmpA.close()
 tmpB.close()
